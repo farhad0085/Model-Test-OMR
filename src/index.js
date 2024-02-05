86f14d0a92b286a0ae088e346ddc7f8cff0614ee
Farhad Hossain <farhadhossain0085@gmail.com>
2024-02-06 03:58:45 +0600

    removed react-router-dom for github pages - it doesn't support routing

--- src/index.js
+++ src/index.js
@@ -4,20 +4,12 @@ import "./index.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "react-confirm-alert/src/react-confirm-alert.css"; // Import react-confirm-alert CSS
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Home />
   </React.StrictMode>
 );
